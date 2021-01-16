9366c700324ababe061f0762c83a762c08187593
ZPanic0 <zc.sgs5@gmail.com>
2021-01-16 02:03:03 -0500

    Merge given start point with CRA template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Instructions:
 
-export default App;
+// 1. Add the functionality to exchange one currency to another 
+//(you can use a technology of your choice) 🤖
+// 2. Style the app 🎨
+
+const App = () => <>
+  <h1>Amazing Currency Converter</h1>
+  <p>
+    Convert
+    <input type="number" id="original-currency-amount" placeholder="1"></input>
+    <input type="text" id="original-currency-unit" placeholder="original currency"></input>
+to
+<input type="text" id="new-currency-unit" placeholder="new currency"></input>
+  </p>
+  <p>
+    Exchange Rate:
+<input type="number" id="exchange-rate"></input>
+  </p>
+  <button>Exchange my money now!</button>
+  <p id="output-text">
+    Converted 💰 will appear here.
+</p>
+
+  <script src="index.pack.js"></script>
+</>
+
+export default App
