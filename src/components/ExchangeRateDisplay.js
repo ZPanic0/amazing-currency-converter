a28bf90e610386290bba4dcc516f1f18a4c77d62
ZPanic0 <zc.sgs5@gmail.com>
2021-01-17 16:07:55 -0500

    Encapsulate exchange rate display

---
+++ src/components/ExchangeRateDisplay.js
@@ -0,0 +1,5 @@
+import { Typography } from '@material-ui/core'
+import React from 'react'
+
+export const ExchangeRateDisplay = ({ exchangeRate }) =>
+    <Typography>Exchange Rate: {exchangeRate}</Typography>
